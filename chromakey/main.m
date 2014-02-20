3ca9bb3de7f407410b236120728b51e4cbeec6ee
Kevin Meaney <ktam@yvs.eu.com>
2014-02-20 15:02:27 +0000

    Initial Commit

---
+++ chromakey/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  chromakey
+//
+//  Created by Kevin Meaney on 20/02/2014.
+//  Copyright (c) 2014 Kevin Meaney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
